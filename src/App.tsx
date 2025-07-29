4a20191639b3e0e662d4bfc22e80cf3013edddf0
Tomiwa Aluko <tomiwaaluko02@gmail.com>
2025-07-29 01:05:52 -0400

    Add Vercel Analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import ScrollToTop from "./components/ScrollToTop";
 import Index from "./pages/Index";
 import Service from "./pages/Service";
@@ -72,6 +73,7 @@ const App = () => (
         </Routes>
         <ScrollToTop />
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
