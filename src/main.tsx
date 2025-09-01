d825009da00236018c3c5e46eff394565e4f49ed
Tomiwa Aluko <tomiwaaluko02@gmail.com>
2025-09-01 17:59:50 -0400

    removed test api for prod

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Test API endpoint (can be removed in production)
-import "./test-resend";
-
 createRoot(document.getElementById("root")!).render(<App />);
